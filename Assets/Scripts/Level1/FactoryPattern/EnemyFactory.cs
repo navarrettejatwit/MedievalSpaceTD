bf8481aa374b7cfd7aff9c57cd9563fe3369201a
navarrettejatwit <46931515+navarrettejatwit@users.noreply.github.com>
2022-07-17 15:22:19 -0400

    MedievalSpaceTD Rebuild

---
+++ Assets/Scripts/Level1/FactoryPattern/EnemyFactory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : Factory
+{
+    private Enemy e;
+
+    private Transform SpawnPoint;
+
+    private GameObject enemies;
+
+    public EnemyFactory(Enemy e, GameObject enemies)
+    {
+        this.e = e;
+        this.enemies = enemies;
+    }
+
+    public Product produce()
+    {
+        return spawn(e, SpawnPoint, enemies);
+    }
+
+    private Product spawn(Enemy e, Transform esp, GameObject enemies)
+    {
+        return Object.Instantiate(e, new Vector3(esp.position.x, esp.position.y, esp.position.z), Quaternion.identity, enemies.transform);
+    }
+
+    public void setSpawnPoint(Transform esp)
+    {
+        SpawnPoint = esp;
+    }
+}
+
---
+++ Assets/Scripts/Level1/TowerScripts/Tower.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour, Product
+{
+
+    [SerializeField] private GameObject Projectiles = null;
+    [SerializeField] private GameObject arrow = null;
+        
+    [SerializeField] private float rateOfFire = 0;
+
+    [SerializeField] private float fireTime = 0;
+    
+    [SerializeField] private int damage = 0;
+    
+    [SerializeField] private int health = 0;
+
+    private int layermask;
+
+    private Ray ray;
+
+    private Enemy e = null;
+
+    void Awake(){
+        layermask = LayerMask.GetMask("Enemies");
+        ray = new Ray(this.transform.position, new Vector3(10,0,0));
+    }
+  
+    void Update()
+    {
+        if (e != null)
+        {
+            this.takeDamage(e.doDamage());
+        }
+
+        RaycastHit hit;
+        Debug.DrawRay(this.transform.position, new Vector3(10,0,0), Color.green);
+        if (Physics.Raycast(ray, out hit, 10f, layermask))
+        {
+            fireTime -= Time.deltaTime;
+            if(fireTime <= 0f){
+                arrow = Instantiate(arrow, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity, Projectiles.transform);
+                arrow.transform.rotation = Quaternion.Euler(0f, 90f,0f);
+                fireTime = rateOfFire;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            e = collision.gameObject.GetComponent<Enemy>();
+        }
+    }
+
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            e.setIsMoving();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private int doDamage()
+    {
+        return this.damage;
+    }
+}
+
+
